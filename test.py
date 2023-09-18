16594832a52502b80887f922791e08ffa36bddeb
mirac <miracgnc53@gmail.com>
2023-09-18 20:29:52 +0300

    .

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
+name = input("İsim gir").strip().title()
+
+ilk, son =  name.split(" ")
+
+print(f" ilk = {ilk}")
+print(f" son = {son}")
+print(f" name = {name}")
